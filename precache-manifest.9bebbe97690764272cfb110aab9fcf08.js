341a4c049ca08dfa1b01b3a7bee8a38d42102a5e
williamxlr <wara65@hotmail.com>
2020-05-13 17:35:07 -0500

    Updates

---
+++ precache-manifest.9bebbe97690764272cfb110aab9fcf08.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f05f28e875cd226792abf1a9e7a2ef5f",
+    "url": "/coin-prices-react/index.html"
+  },
+  {
+    "revision": "ccd113645cedfffb20c3",
+    "url": "/coin-prices-react/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "f26a011ef407fb97bfac",
+    "url": "/coin-prices-react/static/css/main.b062d5e2.chunk.css"
+  },
+  {
+    "revision": "ccd113645cedfffb20c3",
+    "url": "/coin-prices-react/static/js/2.7a551fd7.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/coin-prices-react/static/js/2.7a551fd7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f26a011ef407fb97bfac",
+    "url": "/coin-prices-react/static/js/main.241d475e.chunk.js"
+  },
+  {
+    "revision": "6cdf87d879abb9c459a7",
+    "url": "/coin-prices-react/static/js/runtime-main.9a12a58a.js"
+  }
+]);
